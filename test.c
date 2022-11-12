2db3bc6ba4574126091b5d9d7124a3f1afc06855
adnna <adnanalemranontor@gmail.com>
2022-11-12 16:17:13 +0600

    local all

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+   int a;
+   int b;
+   int c;
+
+   printf("Enter your a number :");
+    scanf("%d",&a);
+
+    printf("enter your b number :");
+    scanf("%d",&b);
+   c =a+b;
+   printf("%d + %d = %d",a,b,c);
+
+}
