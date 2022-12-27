a6ef7686e135256b653ae158d8398cda01b27ebe
adnanalemran <adnanalemranontor@gmail.com>
2022-12-27 11:25:30 +0600

    add more file

--- test.c
+++ test.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-void main()
+
+int main()
 {
-   int a;
-   int b;
-   int c;
-
-   printf("Enter your a number :");
-    scanf("%d",&a);
-
-    printf("enter your b number :");
-    scanf("%d",&b);
-   c =a+b;
-   printf("%d + %d = %d",a,b,c);
+    float a, b;
+    int d, e, c, x=6, y ;
+    printf("increment:\n");
+    y = x++;
+    printf("\t for x++\t x=%d, \t y=%d\n",x,y);
+    getch() ;
+    return 0;
 
 }
